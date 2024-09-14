67bf538c3bdc82894bc5bb62d4b166b5ac747b36
Link <sampersonal01@outlook.com>
2024-09-14 18:21:54 -0300

    Commit inicial do projeto.

---
+++ src/main/kotlin/br/com/projectbank/ProjectBankApplication.kt
@@ -0,0 +1,11 @@
+package br.com.projectbank
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjectBankApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjectBankApplication>(*args)
+}
---
+++ src/main/kotlin/br/com/projectbank/ServletInitializer.kt
@@ -0,0 +1,12 @@
+package br.com.projectbank
+
+import org.springframework.boot.builder.SpringApplicationBuilder
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+
+class ServletInitializer : SpringBootServletInitializer() {
+
+	override fun configure(application: SpringApplicationBuilder): SpringApplicationBuilder {
+		return application.sources(ProjectBankApplication::class.java)
+	}
+
+}
